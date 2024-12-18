61627b7d54203e38017a2286f01168ecfc3a4194
Szymon-2001 <sz.bryczek@gmail.com>
2024-12-18 18:30:52 +0100

    tried to list the order but it dois not work

--- sql/user.sql
+++ sql/user.sql
@@ -4,8 +4,11 @@ CREATE TABLE User (
     password VARCHAR(255) NOT NULL,
     role ENUM('customer', 'admin') NOT NULL DEFAULT 'customer',
     isLogged BOOLEAN NOT NULL DEFAULT FALSE
+    isBlocked BOOLEAN NOT NULL DEFAULT FALSE
 );
 INSERT INTO User (name, password, role)
 VALUES 
 ('John123', 'testpass123', 'customer'),
 ('Bob123', 'testpass123', 'admin');
+
+
